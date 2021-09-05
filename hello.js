d6c79c5be6e10d79959dc1d71aa55ece76c4d401
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-05 20:24:35 +0600

    js practice files

---
+++ hello.js
@@ -0,0 +1,3 @@
+console.log(100)
+console.log(100 + 200)
+console.log(500 - 300)
---
+++ variable.js
@@ -0,0 +1,26 @@
+// Number variable 
+var bananaPrice = 12;
+console.log(bananaPrice);
+
+var rabbi = 21;
+console.log(rabbi)
+
+var a = 50;
+var b = 40;
+console.log(a + b);
+
+var c = 60;
+var d = 50;
+console.log(c * d)
+    //string variable
+
+var name = "Md. Mahfuzur Rahman";
+console.log(name);
+var fatherName = "Abdus Salam";
+console.log(fatherName);
+
+//boolean variable
+var mywifeTalk = false;
+var me = true;
+console.log(mywifeTalk);
+console.log(me);
