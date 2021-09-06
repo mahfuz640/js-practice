355a82c9673e73b3c3466d5c6c35860cfe49cbd5
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-06 20:48:05 +0600

    string

--- string.js
+++ string.js
@@ -1,3 +1,7 @@
 // nicher quotetion ar moddhe potita latter ak ak ta string.
 var Bangladesh = "amr bangladesh dekhte onk shundor ami amar desh k bhalobashi amr desh nodimartik desh shobuj foshol ar desh";
 console.log(Bangladesh);
+
+// soto hat ar okkhor a tolowerCase() keyword use hoi.
+var Bangladesh = "amr bangladesh dekhte onk shundor ami amar desh k bhalobashi amr desh nodimartik desh shobuj foshol ar desh";
+console.log(Bangladesh.toLowerCase());
