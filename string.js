e9a3aa9b29767972427061adf2fa38eb49e8b20a
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-06 20:43:30 +0600

    about of Bangladesh

---
+++ string.js
@@ -0,0 +1,3 @@
+// nicher quotetion ar moddhe potita latter ak ak ta string.
+var Bangladesh = "amr bangladesh dekhte onk shundor ami amar desh k bhalobashi amr desh nodimartik desh shobuj foshol ar desh";
+console.log(Bangladesh);
