46bc3999333e45bf5e431ab15945d1f4b0c73fa0
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-06 21:14:47 +0600

    string practice

--- string.js
+++ string.js
@@ -9,3 +9,8 @@ console.log(Bangladesh.toLowerCase());
 // boro hat ar okkhor a toUpperCase() keyword use hoi.
 var Bangladesh = "amr bangladesh dekhte onk shundor ami amar desh k bhalobashi amr desh nodimartik desh shobuj foshol ar desh";
 console.log(Bangladesh.toUpperCase());
+
+//kono String ar moddhe theke kono latter ar obostha ba position thake ta koto tomo ta bar kota jai indexOf diye.
+var Bangladesh = "amr bangladesh dekhte onk shundor ami amar desh k bhalobashi amr desh nodimartik desh shobuj foshol ar desh";
+console.log(Bangladesh.indexOf("dekhte")); //output 15 this is position
+console.log(Bangladesh.indexOf("onk")); //output 22 this is position
