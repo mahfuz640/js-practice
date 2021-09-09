8c4fd10e1a37b9b36eddbc14d7afc3685806f38d
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-09 17:37:49 +0600

    js practice

--- operator.js
+++ operator.js
@@ -15,3 +15,21 @@ var num3 = 80;
 var num4 = 10;
 var result = num3 / num4;
 console.log(result);
+
+//modulas vhagshesh operator practice
+var num3 = 70;
+var num4 = 10;
+var result = num3 % num4;
+console.log(result);
+
+//amra jodi variable ar number ar man 1 barate chai tao same variable a tokhon amra ++ increment use kori
+var num5 = 80;
+var num6 = 50;
+num6++;
+console.log(num6);
+
+//amra jodi variable ar number ar man 1 komate chai tao same variable a tokhon amra --decrement use kori
+var num7 = 80;
+var num8 = 50;
+num8--;
+console.log(num8);
