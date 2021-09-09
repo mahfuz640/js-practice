c57858b513e8432d7cc903136ab68cf46f0b487b
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-09 16:18:05 +0600

    multiple

--- operator.js
+++ operator.js
@@ -3,3 +3,9 @@ var alu = 20;
 var peyaz = 35;
 var totalBazar = alu + peyaz;
 console.log(totalBazar)
+
+//multiple operator practice
+var num1 = 5;
+var num2 = 3;
+var result = num1 * num2;
+console.log(result)
