b5dcbb0ab68c0464763ca0fe77baf244bd815e72
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-09 12:40:42 +0600

    int float sting

--- int float sting.js
+++ int float sting.js
@@ -2,3 +2,9 @@
 var number1 = 50;
 var number2 = 52.6; //this is a float/decimal number 
 console.log(number1 + number2);
+
+//jodi kono shongkha string a thake take jevabe shonkhai convert korte hobe 
+var number1 = 50;
+var number2 = '52.6';
+number2 = parseFloat(number2);
+console.log(number1 + number2);
