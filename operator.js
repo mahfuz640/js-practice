f1fe41450068965b636042a7bc6e2af2420477ed
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-09 17:36:48 +0600

    js practice

--- operator.js
+++ operator.js
@@ -9,3 +9,9 @@ var num1 = 5;
 var num2 = 3;
 var result = num1 * num2;
 console.log(result)
+
+// division operator practice
+var num3 = 80;
+var num4 = 10;
+var result = num3 / num4;
+console.log(result);
