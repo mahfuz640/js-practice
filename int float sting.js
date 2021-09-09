a532540ed73c660a636552046d5ebe6607156950
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-09 12:54:23 +0600

    parsefloat

--- int float sting.js
+++ int float sting.js
@@ -6,5 +6,6 @@ console.log(number1 + number2);
 //jodi kono shongkha string a thake take jevabe shonkhai convert korte hobe 
 var number1 = 50;
 var number2 = '52.6';
-number2 = parseFloat(number2);
-console.log(number1 + number2);
+number2 = parseFloat(number2); //kono variable string type a thakle take number a shte jog/gun/vag/biyog korar time usekora hoi
+//parseFloat float type number ar khetre use kora hoi --ar int type number thakle parseInt use kora hoi
+console.log(typeof number2);
