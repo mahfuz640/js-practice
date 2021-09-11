47fbc10fd6f56630fb76604f4194d65b16c123de
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-11 20:14:03 +0600

    condition

--- condition if else.js
+++ condition if else.js
@@ -1,6 +1,17 @@
+//single type conditional logic
 var biscuitPrice = 10;
 if (biscuitPrice < 12) {
     console.log("ami biscuit kinbo");
 } else {
     console.log("ami biscuit khabo na");
+}
+
+//multiple type conditional logic for
+//jodi duti conditoin akotre thake tokhon && symbol boshe.ar jokhon duti shorto puron korsbe tokhon e output a if ar result show korbe.
+var jobPaiso = true;
+var moneysaving = 500000;
+if (jobPaiso = true && moneysaving > 200000) {
+    console.log("Asho tmr patri dekhi");
+} else {
+    console.log("tor kopale bou nai")
 }
