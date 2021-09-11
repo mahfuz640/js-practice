638fe6675840c39052a24f9c7f45abfade3f95d9
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-11 15:28:56 +0600

    round

--- math.js
+++ math.js
@@ -2,3 +2,8 @@
 var number = -5;
 var absoluteNumber = Math.abs(number);
 console.log(absoluteNumber);
+
+//round holo kono shongkha jodi 0.5 theke0.9 hoi tahole she poroborti purno shongkha prokash korbe
+var number = 42.445;
+var round = Math.round(number);
+console.log(round);
