18b512249b7a3496562e8d06c2bc1877cc4d77bc
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-11 23:24:44 +0600

    marks grade

---
+++ mark grad.js
@@ -0,0 +1,11 @@
+var bangla = 98;
+var english = 100;
+var Math = 100;
+var ICT = 100;
+if (bangla > 80 && english > 80 && Math > 80 && ICT > 60) {
+    console.log("A+")
+} else if (bangla < 80 && english < 80 && Math < 80 && ICT < 60) {
+    console.log("A")
+} else {
+    console.log("Failed")
+}
