a3c84c0ea619f53b0181e87d37e6f9e0a820fcbb
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-11 20:20:54 +0600

    condition

--- condition if else.js
+++ condition if else.js
@@ -9,9 +9,28 @@ if (biscuitPrice < 12) {
 //multiple type conditional logic for
 //jodi duti conditoin akotre thake tokhon && symbol boshe.ar jokhon duti shorto puron korsbe tokhon e output a if ar result show korbe.
 var jobPaiso = true;
-var moneysaving = 500000;
+var moneysaving = 50000;
 if (jobPaiso = true && moneysaving > 200000) {
     console.log("Asho tmr patri dekhi");
 } else {
     console.log("tor kopale bou nai")
+}
+
+// || ay symbol diye othoba bujhai jodi je kono akta true hoi tokhon e if ar line print hobe. 
+var jobPaiso = true;
+var moneysaving = 50000;
+if (jobPaiso = true || moneysaving > 200000) {
+    console.log("Asho tmr patri dekhi");
+} else {
+    console.log("tor kopale bou nai")
+}
+//jodi akadik condition thake tokhon amra else if use korte pari..
+var jobPaiso = true;
+var moneysaving = 50000;
+if (jobPaiso = true && moneysaving > 200000) {
+    console.log("Asho tmr patri dekhi");
+} else if (jobPaiso = true) {
+    console.log("aro kisu din shomai nao");
+} else {
+    console.log("tor kopale bou nai");
 }
