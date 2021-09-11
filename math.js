28146f62547416e3a6a75de3a6762db0fc5fdbe2
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-11 15:41:10 +0600

    celi

--- math.js
+++ math.js
@@ -4,6 +4,12 @@ var absoluteNumber = Math.abs(number);
 console.log(absoluteNumber);
 
 //round holo kono shongkha jodi 0.5 theke0.9 hoi tahole she poroborti purno shongkha prokash korbe
+//ar jodi ta 0.1-0.4 ar je shongkhar shate doshomic take purno shogkha kore dibe
 var number = 42.445;
 var round = Math.round(number);
 console.log(round);
+
+//kono purno shongkha ar por jodi shamanno beshi thake tahole take baiye porborti purno shongkhai rupanttor hobe
+var number = 5.0009;
+var result = Math.ceil(number);
+console.log(result);
