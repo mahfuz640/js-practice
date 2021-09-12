ac9cf9d04e9ce29d58970ccb02f3e67e4f6ed13f
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-13 00:21:32 +0600

    array basic

--- array.js
+++ array.js
@@ -20,3 +20,8 @@ console.log(age[0])
 var age = [12, 50, 44, 66]
 var num1Position = age.indexOf(50)
 console.log(num1Position)
+
+//ar jodi kono array list ar bahirer kono number ar position ber kori tokhon ta amader -1 output dekhabe
+var age = [12, 50, 44, 66]
+var num1Position = age.indexOf(556)
+console.log(num1Position)
