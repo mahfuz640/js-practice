c77083031006233a513bed07f03a7e8743e0c025
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-13 00:10:49 +0600

    array

--- array.js
+++ array.js
@@ -5,3 +5,8 @@ console.log(age);
 //position shobshomai bam dik diye shuru hoi 0,1,2,3
 var age = [12, 50, 44, 66]
 console.log(age[2]);
+
+//amra chaile array ar position variable o rakhte pari
+var age = [12, 50, 44, 66]
+var num1Position = age[0];
+console.log(num1Position);
