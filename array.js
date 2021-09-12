ed8a6de2355f4969428dd1872b473c74eb2830d8
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-13 00:14:24 +0600

    array

--- array.js
+++ array.js
@@ -10,3 +10,8 @@ console.log(age[2]);
 var age = [12, 50, 44, 66]
 var num1Position = age[0];
 console.log(num1Position);
+
+//kono array ar ager position ar number na muse oy position a new number add kora
+var age = [12, 50, 44, 66]
+age[0] = 52;
+console.log(age[0])
