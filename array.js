48f167f5616728d853d3e1af62d0caaa1dfe6825
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-13 00:07:20 +0600

    array

--- array.js
+++ array.js
@@ -1,2 +1,7 @@
 var age = [12, 50, 44, 66];
 console.log(age);
+
+//array ar position ber korar jonno indexOf use kora hoi
+//position shobshomai bam dik diye shuru hoi 0,1,2,3
+var age = [12, 50, 44, 66]
+console.log(age[2]);
