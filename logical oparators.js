8c94a84d1c73c1a1dd3a52ad5376877080a08391
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-13 23:03:40 +0600

    logical operator not

---
+++ logical oparators.js
@@ -0,0 +1,14 @@
+// not operator practice
+var chor = true;
+if (chor != false) {
+    console.log("she chor ")
+} else {
+    console.log("she chor na")
+}
+
+var chor = true;
+if (chor != true) {
+    console.log("she chor ")
+} else {
+    console.log("she chor na")
+}
