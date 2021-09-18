9f66fbf0ce741af81a94abd31dda493fa6e63603
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-18 10:38:15 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,19 @@
+var num = 6;
+switch (num) {
+    case 1000:
+        console.log("Bangladesh")
+        break;
+
+    case 2000:
+        console.log("India")
+        break;
+    case 3000:
+        console.log("China")
+        break;
+
+    case 6:
+        console.log("Srilanka")
+        break;
+    default:
+        console.log("i donot know your country")
+}
