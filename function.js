8904cfeb7895f1da26facefb5f6e6237f6f80814
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-18 10:44:34 +0600

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+function me() {
+    console.log("Md. Mahfuzur Rahman")
+}
+
+//function ar maddhome ami code ar jekhane e thaki na keno ami tokhone take call korle she hazir hobe
+
+var a = 5
+var b = 6
+console.log(a * b)
+
+me()
