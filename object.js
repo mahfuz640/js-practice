11283ac5fd6148d1487ec76c23566fcaa535a53a
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-18 17:09:29 +0600

    object update

--- object.js
+++ object.js
@@ -1,8 +1,18 @@
 var student = { id: 001, name: "Mahfuz", phone: 1234 };
 var student2 = { id: 002, name: "Salam", phone: 4567 };
-// console.log(student);
-// console.log(student2);
+console.log(student);
+console.log(student2);
 
 //tumi ay list theke kono special item jante chao tokhon
 var phoneNo = student.phone
+console.log(phoneNo);
+
+// or
+
+var phoneNo = student["phone"];
+console.log(phoneNo);
+
+// or
+var proName = 'phone'
+var phoneNo = student['phone']
 console.log(phoneNo);
