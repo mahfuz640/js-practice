b5c3ea1a1b9c6ff52a294b1df6cfb0203e42603b
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-18 12:36:05 +0600

    functionlan add

--- function.js
+++ function.js
@@ -9,3 +9,34 @@ var b = 6
 console.log(a * b)
 
 me()
+
+// kono kisu ke double kora
+function double(num) {
+    var result = num * 2;
+    console.log(result);
+}
+double(50)
+double(500)
+double(10)
+double(20)
+double(30)
+
+// namta
+function namta(num) {
+
+    var result = num * 2;
+    return result;
+}
+// return ar jonno manta k amra var ar moddhe rekhese
+var first = namta(5)
+var second = namta(6)
+total = first * second
+console.log(total)
+
+//var of function
+function add(num1, num2) {
+    var result = num1 + num2;
+    return result;
+}
+var additional = add(1, 2);
+console.log(additional)
