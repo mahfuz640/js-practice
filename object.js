245d18cdb23230d95fb25ad377ba710c92cd57cf
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-18 16:59:39 +0600

    object

---
+++ object.js
@@ -0,0 +1,6 @@
+var student = { id: 001, name: "Mahfuz", phone: 1234 };
+var student2 = { id: 002, name: "Salam", phone: 4567 };
+console.log(student);
+console.log(student2);
+
+//tumi ay list theke kono special item jante chao tokhon se
