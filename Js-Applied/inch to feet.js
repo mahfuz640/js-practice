af2288dfa58e1ec6f6cc020efa50e48d1d9a6c87
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-19 12:09:59 +0600

    inch to feet

--- Js-Applied/inch to feet.js
+++ Js-Applied/inch to feet.js
@@ -7,3 +7,8 @@ var nanarFeet = inchToFeet(156)
 console.log(nanarFeet)
 var nanirFeet = inchToFeet(154)
 console.log(nanirFeet)
+
+//or
+var feetList = [156, 288, 300]
+var nanafeet = inchToFeet(feetList[1]);
+console.log(nanafeet)
