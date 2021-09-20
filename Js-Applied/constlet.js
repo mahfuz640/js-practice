eb27bdb1a3af1c612b99c1eed90d3e9bd582fb95
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-20 11:25:54 +0600

    const and let

---
+++ Js-Applied/constlet.js
@@ -0,0 +1,14 @@
+var
+// ata shadharon vhabe use hoi basic program a
+
+
+const
+// const holo amon spicial keyword ja variable ar man change kore na fix thake  chaile change kora jai na but conditon a conditon deye jai
+
+
+
+let
+
+// let holo amon akta keyword ja variable ar man change kore  fix o chaile rakha jai
+
+// let k const ar jaiga use kora jai but const k let ar jaiga use kora jai na
