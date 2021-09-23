5ff6ae6268b4ec084c12fe2e74d8733649ef7c39
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-23 15:58:51 +0600

    swap

--- provlemsolving/swap.js
+++ provlemsolving/swap.js
@@ -6,4 +6,13 @@ console.log("before swap :a=", a, "b=", b)
 var temp = a;
 a = b;
 b = temp;
-console.log('after swap::a=', a, "b=", b)
+console.log('after swap:a=', a, "b=", b)
+
+// rules:2 with out extra variable
+var x = 5
+var y = 7
+console.log('before swap:x=', x, "y=", y)
+x = x + y
+y = x - y
+x = x - y
+console.log('after swap:x=', x, "y=", y)
