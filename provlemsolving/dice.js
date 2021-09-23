f94ac51d9f0d4b5bf4c8a6471da0c7fb0dd4a1bb
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-23 23:17:48 +0600

    dice

---
+++ provlemsolving/dice.js
@@ -0,0 +1,10 @@
+var randomNum = Math.random() * 6
+var output = Math.round(randomNum)
+console.log(output)
+
+// amra chaile loop bebohar kore dice ar shongkha gulo ber korte parisi
+for (var i = 0; i < 6; i++) {
+    var randomNum = Math.random() * 6
+    var output = Math.round(randomNum)
+    console.log(output)
+}
