cd7bb4847cfb0d643a7226879028fa30a16eefa8
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-25 00:21:40 +0600

    array sum

---
+++ provlemsolving/arraysum.js
@@ -0,0 +1,7 @@
+var numbers = [45, 65, 78, 12, 3, 54, 65]
+var sum = 0;
+for (var i = 0; i < numbers.length; i++) {
+    var elements = numbers[i];
+    sum = sum + elements;
+}
+console.log('total of the number:', sum);
