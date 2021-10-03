f05a65d3e7ee057d3a77c5b0baf60a36f22bf028
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-03 14:19:00 +0600

    function basic

--- js basic/function basic.js
+++ js basic/function basic.js
@@ -5,3 +5,12 @@ function add(num1, num2) {
 var result = add(45, 5) //add nam ar akta function create kore pore amra abar call kortese thn chaile jog biyog gun bhag shob korte pari
 
 console.log(result)
+
+//function diye array ar 
+function list(n) {
+    for (var i = 0; i < n.length; i++) {
+        var element = n[i]
+    }
+}
+list = ([45, 666, 522])
+console.log(list)
