7421a1fd73085947dc87e762876fbb51330171be
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-03 11:44:26 +0600

    basic js

---
+++ js basic/js basic1.js
@@ -0,0 +1,12 @@
+var num = 59;
+num = 75 // akahne amra je variable ar man last jeta dibo sheta e hobe 
+console.log(num)
+
+let nu = 60 // akahne amra je variable ar man chaile change korte parbo
+nu = 45
+console.log(nu)
+    //but but but const amon akta keyword jar moddhe amra kono variable rakle tar man amra change korte parbo na 
+const noChange = 12
+    // noChange = 55 // ata korle amader error ashbe so ata kora jabe na
+console.log(noChange)
+    // amra shudu number type na amra chaile string a o same jinish use  korte pari
