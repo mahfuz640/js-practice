2332c1559b9c1c7875f4d3295a5e24a60f05c5be
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-06 12:27:24 +0600

    return

---
+++ js basic/files 19/return.js
@@ -0,0 +1,8 @@
+function evenify(num) {
+    if (num % 2 == 0) {
+        console.log('this is a even Number', num)
+    } else {
+        console.log('this is odd number with double', num * 2);
+    }
+}
+evenify(5)
