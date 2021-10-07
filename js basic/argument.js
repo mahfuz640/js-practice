0ee94af8d54fee32a5cdcdb9cb0c26d216cd53bc
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-07 21:36:59 +0600

    argument

--- js basic/argument.js
+++ js basic/argument.js
@@ -1,5 +1,16 @@
+// function addNumber(num1, num2) {
+//     return num1 + num2;
+// }
+// var result = addNumber(3, 5);//num1=3,num2=5avabe declear kore
+// console.log(result);
 function addNumber(num1, num2) {
-    return num1 + num2;
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        const num = arguments[i];
+        sum = sum + num
+    }
+    // arguments.push(10); argument push kora jai na array ar moto
+    return sum;
 }
-var result = addNumber(3, 5);
-console.log(result);
+var result = addNumber(3, 5, 8, 15)
+console.log(result)
