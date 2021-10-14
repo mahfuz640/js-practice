373e57b4df9256daf7344c5f1f667e95a1d223bb
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-14 13:19:08 +0600

    revision

--- js basic/files 19/revision.js
+++ js basic/files 19/revision.js
@@ -1,3 +1,35 @@
 console.log(12);
 // array
-nums = [5, 23, 6, 5, 6, 51, ]
+nums = [5, 23, 16, 5, 6, 51, ]
+for (i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    // odd gulo k 
+    if (num % 2 == 0) {
+        console.log(num, ' --Is a Even number')
+    } else {
+        console.log(num, ' --Is a Odd number')
+    }
+}
+// friend age loop
+friend_age = [4, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+for (let i = 0; i < friend_age.length; i++) {
+    const age = friend_age[i];
+    if (age % 2 == 0) {
+        console.log(age, 'is even number')
+    } else {
+        console.log(age, 'is odd number')
+    }
+
+}
+
+function give(num) {
+
+    if (num % 2 == 0) {
+        console.log(num, 'is even number')
+    } else {
+        console.log(num, 'is odd number')
+    }
+
+}
+give(2)
