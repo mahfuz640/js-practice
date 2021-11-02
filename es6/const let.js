b2fd7f9ee0e2fb96e4b5899da60d05f30e3d6e02
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-11-02 10:33:22 +0600

    node

--- es6/const let.js
+++ es6/const let.js
@@ -1,3 +1,22 @@
 const hubby = "Elias kanchon"
-    // hubby = "Omar sunny" //const akbar e declear korle set hoye jaiæ
+    // hubby = "Omar sunny" //const akbar e declear korle set hoye jai pore ar change kora jai na
 console.log(hubby)
+
+// array of const
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(123);
+// numbers = ["sunny", "salam", "omit hasan"]
+// aki variable a const set korar por a ar update kora jabe na
+console.log(numbers)
+
+// jodi change kora lage tokhon amara let use korbo
+let patientName = "Rahim chacha"
+patientName = "Fatema khala"
+console.log(patientName)
+
+// loop used let and const
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+
+}
