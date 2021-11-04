d1451f6092dcc10538abdfcc3aadb2dc51a10645
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-11-04 13:59:00 +0600

    templete

--- es6/template.js
+++ es6/template.js
@@ -9,3 +9,13 @@ const fullName2 = `${firstName} ${lastName} is a good boy.`;
 console.log(fullName2);
 
 // age amra old verstion user programmer jeveave likhto
+
+const multiLine = "amma" +
+    "abba" +
+    "ami";
+console.log(multiLine);
+
+// same akhon amra jodi caret use kori tahole dekhbo je agula akotre add na hoye alada alada add hoyese
+
+const multiLine2 = `abba amma ami`
+console.log(multiLine2);
