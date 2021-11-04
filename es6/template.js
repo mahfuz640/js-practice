dcdeb16216a807b42e70c38043c054ef26acdbef
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-11-04 11:07:22 +0600

    templete

---
+++ es6/template.js
@@ -0,0 +1,11 @@
+const firstName = "justin";
+const lastName = "trudo";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// caret -->  `` ata moddhe kono string likhle multiline bujte pare 
+//template-->${}
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+console.log(fullName2);
+
+// age amra old verstion user programmer jeveave likhto
